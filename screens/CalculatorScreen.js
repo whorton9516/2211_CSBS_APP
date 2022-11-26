4f20b50b4ba67a487aff6f1012d6b53d4aaa2f2f
whorton9516 <samwhorton@gmail.com>
2022-11-26 12:48:07 -0500

    Updated CalculatorScreen.js so now the page is much more basic

--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -1,36 +1,17 @@
-import { StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
-export default function CalculatorScreen() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Calculator</Text>
-        <View
-          style={styles.separator}
-          lightColor="#eee"
-          darkColor="rgba(255,255,255,0.1)"
-        />
-        <EditScreenInfo path="/screens/CalculatorScreen.tsx" />
-      </View>
-    );
-  }
+const CalculatorScreen = () => {
+  return <Text style={styles.textStyle}>Calculator Screen</Text>;
+};
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: "80%",
-    },
-  });
-  
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30
+  }
+});
+
+export default CalculatorScreen;
