4bf380c41bdd319a0a146458e7e9f7072934164c
whorton9516 <samwhorton@gmail.com>
2023-01-12 17:54:27 -0500

    Working changes to SQLite and Quizzes

--- App.js
+++ App.js
@@ -10,20 +10,23 @@ import * as SQLite from 'expo-sqlite';
 import { useState, useEffect } from 'react';
 
 export default function App() {
+  console.log('App Started');         // Print to the terminal while debugging
   const [isLoading, setIsLoading] = useState(true);
   const isLoadingComplete = useLoadedAssets();
   const colorScheme = useColorScheme();
   const [colorTheme, setColorTheme] = useState(0);
   const [darkMode, setDarkMode] = useState(true);
-  const [colorBlindMode, setColorBlindMode] = useState(0)
+  const [colorBlindMode, setColorBlindMode] = useState(0);
 
-  const db = SQLite.openDatabase('userSettings.db');
+  const db = SQLite.openDatabase('userData.db');
+  
   // Create a table to store the user preferences if it doesn't exist
   useEffect(() => {
 
     db.transaction(tx => {
       tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS userSettings (colorTheme INTEGER, ColorBlindMode INTEGER, DarkMode BOOLEAN);'
+        'CREATE TABLE IF NOT EXISTS userSettings (colorTheme INTEGER, ColorBlindMode INTEGER, DarkMode BOOLEAN);',
+        'CREATE TABLE IF NOT EXISTS userData (id INTEGER);'
       );
     });
   }, []);
--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View, StatusBar, TextInput, Keyboard, Dimensions, SafeAreaView } from "react-native";
 import CustomButton from "../components/CustomButton";
 import { Col, Row, Grid } from "react-native-easy-grid";
+import { ConsoleSqlOutlined } from "@ant-design/icons";
 
 const {width, height} = Dimensions.get('window');
+let totalQuestions = 0;
+let correct = 0;
 
 export default function QuizScreen () {
-    const [defaultStyle, setDefaultStyle] = useState(true);
+  console.log('Quiz Screen Opened');
+  const [defaultStyle, setDefaultStyle] = useState(true);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <TextInput
-        style={styles.TextDisplay}
-        placeholder="Question here"
-        textAlign='center'
-        keyboardType="numeric"
-        color='white'
-        placeholderTextColor='white'
-        editable={false}
-      ></TextInput>
+      <Text style={styles.TextDisplay}>
+        
+        {question.str}
+
+      </Text>
             <TextInput
         style={styles.TextInput}
         placeholder="Answer here"
@@ -26,6 +27,7 @@ export default function QuizScreen () {
         keyboardType="numeric"
         color='white'
         placeholderTextColor='white'
+        editable={true}
       ></TextInput>
       <Grid>
     <Row></Row>
@@ -47,6 +49,39 @@ export default function QuizScreen () {
     </View>
   );
 }
+
+function GenerateQuestion(){
+  const question = {
+  num1: (Math.floor(Math.random() * 10) + 1),
+  num2: (Math.floor(Math.random() * 10) + 1),
+  sym: (Math.floor(Math.random() * 4) + 1),
+  str: num1 + " " + sym + " " + num2,
+  ans: 0
+  };
+
+  // This code is gross! Clean it up
+  switch(question.sym) {
+    case 1:
+      question.ans = question.num1 + question.num2;
+    case 2:
+      question.ans = question.num1 - question.num2;
+    case 3:
+      question.ans = question.num1 * question.num2;
+    case 4:
+      question.ans = question.num1 / question.num2;
+  }
+
+  return question;
+}
+
+function CheckAnswer(answer, number) {
+  if (number == answer) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
