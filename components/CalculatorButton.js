13501dff47f9c4548ee98746524465b4cd9ff97e
whorton9516 <samwhorton@gmail.com>
2023-01-26 17:35:56 -0500

    Multiple changes: * buttons now return release position * equation is built in sync with drag gestures

--- components/CalculatorButton.js
+++ components/CalculatorButton.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native'
 import Draggable from 'react-native-draggable';
+import { Layout } from 'react-native-reanimated';
 
-const CalculatorButton = props => {
-
-    const releasePosition = 100;
-
-    function handleRelease() {
-        console.log('Button Released')
-    };
-
+const CalculatorButton = ({x, y, value, handleRelease}) => {
 
     return (
         <View>
             <Draggable 
-                x= {props.x}
-                y= {props.y}
-                renderText= {props.value}
+                x= {x}
+                y= {y}
+                renderText= {value}
                 renderColor='blue'
                 shouldReverse
                 isCircle
-                // onRelease= {handleRelease}
+                onDragRelease= {(e, gesture) => handleRelease(value, gesture.moveX, gesture.moveY - 50)}
                 />
             
         </View>
--- screens/TestingScreen.js
+++ screens/TestingScreen.js
@@ -1,36 +1,60 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useReducer} from 'react';
 import { Text, View, StyleSheet, PanResponder } from 'react-native';
 import CalculatorButton from '../components/CalculatorButton';
 
 const TestingScreen = props => {
-    const [button, setButton] = useState({x:125, y:400});
-    const panResponder = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: (e, gesture) => true,
-            onPanResponderRelease: (e, gesture) => {
-                console.log('Button was released at: '+  gesture.moveX + ' ' + gesture.moveY);
-            },
-        })
-    ).current;
+
+    const [equation, setEquation] = useState('');
+
+    const GetPosition = (value, xRel, yRel) => {
+        console.log('button ' + value + ' released at x:' + xRel + ' y:' + yRel);
+        if (yRel < 200){
+            console.log('button ' + value + ' was logged');
+            setEquation(equation + value + ' ');
+            console.log(equation);
+        }
+    }
 
     return (
-        <View {...panResponder.panHandlers}>
-            <CalculatorButton x= {button.x} y= {button.y} value= {1} />
-            <CalculatorButton x= {button.x + 75} y= {button.y} value= {2} />
-            <CalculatorButton x= {button.x+ 150} y= {button.y} value= {3} />
-            <CalculatorButton x= {button.x} y= {button.y + 75} value= {4} />
-            <CalculatorButton x= {button.x + 75} y= {button.y + 75} value= {5} />
-            <CalculatorButton x= {button.x + 150} y= {button.y + 75} value= {6} />
-            <CalculatorButton x= {button.x} y= {button.y + 150} value= {7} />
-            <CalculatorButton x= {button.x + 75} y= {button.y + 150} value= {8} />
-            <CalculatorButton x= {button.x + 150} y= {button.y + 150} value= {9} />
-            <CalculatorButton x= {button.x} y= {button.y + 225} value= {10} />
-            <CalculatorButton x= {button.x + 75} y= {button.y + 225} value= {11} />
-            <CalculatorButton x= {button.x + 150} y= {button.y + 225} value= {12} />
+        <View>
+            <View style={styles.dropZone}> 
+                <Text style={styles.text}>{equation}</Text>
+            </View>
+
+            <CalculatorButton x= {125} y= {200} value= {1} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {200} y= {200} value= {2} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {275} y= {200} value= {3} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {125} y= {275} value= {4} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {200} y= {275} value= {5} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {275} y= {275} value= {6} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {125} y= {350} value= {7} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {200} y= {350} value= {8} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {275} y= {350} value= {9} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {125} y= {425} value= {10} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {200} y= {425} value= {11} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {275} y= {425} value= {12} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {375} y= {200} value= {'+'} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {375} y= {275} value= {'-'} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {375} y= {350} value= {'*'} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
+            <CalculatorButton x= {375} y= {425} value= {'/'} handleRelease={(num, xRel, yRel) => GetPosition(num, xRel, yRel)} />
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    dropZone: {
+        height: 200,
+        backgroundColor: "white"
+      },
+    text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center",
+    color: "black",
+    fontSize: 25,
+    fontWeight: "bold"
+    }
+});
 
 export default TestingScreen;
